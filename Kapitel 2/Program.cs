8fb5b4c05742824cf11c454473b78856360fdd50
Admin <Admin@TEB123004>
2023-09-15 15:24:05 +0200

    Add project files.

---
+++ Kapitel 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken stad kommer du ifrån?");
+            string Stad = Console.ReadLine();
+            Console.WriteLine("vad är ditt drömresmål?");
+            string resmål = Console.ReadLine();
+            Console.WriteLine("hej " + Stad + "sbo. Jag hoppas att du får resa till " + resmål +".");
+        }
+    }
+}
